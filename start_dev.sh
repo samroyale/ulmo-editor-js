b4018ab1960cf238f65fdbbbd092a53609dda433
seldred <samuel.eldred@anaplan.com>
2018-01-04 10:01:16 +0000

    Making sure it's still possible to run without docker.

--- start_dev.sh
+++ start_dev.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Starts the dev environment via docker-compose
+
+# Use the 'clean' argument to revert any data changes
 if [ "$1" == "clean" ]; then
     docker-compose rm -f db
     rm -rf db/data
